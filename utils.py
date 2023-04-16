f2d06d6a27bc6e60525d4c9e9b4fca67fda7546f
SmokierLemur51 <ldl6147@gmail.com>
2023-04-16 18:24:06 -0400

    api

--- utils.py
+++ utils.py
@@ -1,7 +1,12 @@
 
 
-def income():
+def income(obj):
+	''' was thinking pass the object model as paramter 
+		and calculate values through that
+	''' 
+
 	return "income"
 
 def expenses():
 	return "expenses"
+
